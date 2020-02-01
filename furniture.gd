fb0ec84e7e4ad7e464b7245d7f2d01a7b96c788d
haininz <haininz@uci.edu>
2020-02-01 15:47:40 -0800

    furniture

--- furniture.gd
+++ furniture.gd
@@ -1,17 +1,28 @@
 extends StaticBody2D
 var is_on_fire = false
 var can_off_fire = false
+const fire_num = 3
+var temp = randi()%2+1
 var ms
 var s
+var s2
+var ms2
 	
 func _process(delta):
 	if ms > 9:
 		s += 1
+		ms = 0
+	if ms2 > 9:
+		s2 += 1
+		ms2 = 0
+		temp = randi()%2+1
+		if temp == fire_num:
+			on_fire()
 	if s >= 40:
 		pass
 	if is_on_fire == false:
 		ms = 0
-		s = 0		
+		s = 0	
 	
 func on_fire():
 	is_on_fire = true
@@ -27,3 +38,6 @@ func off_fire():
 func _on_Timer_timeout():
 	if is_on_fire == true:
 		ms += 1
+
+func _on_Timer2_timeout():
+	ms2 += 1
