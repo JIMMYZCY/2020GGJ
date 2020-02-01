7db608f7aafa0e89e82c60992e1433696164d718
JIMMYZCY <zcy1019877115@gmail.com>
2020-02-01 11:30:02 -0800

    add rotation

--- PlayerController.gd
+++ PlayerController.gd
@@ -7,12 +7,15 @@ var velocity = Vector2()
 
 func _physics_process(delta):
 	get_input()
+	rotation = velocity.angle()
 	move_and_slide(velocity)
+	velocity.normalized()
 
 
 func get_input():
 	var x_dir = 0
 	var y_dir = 0
+	var rotation_dir = 0 
 	if Input.is_action_pressed("ui_left"):
 		x_dir -= 1
 	if Input.is_action_pressed("ui_right"):
